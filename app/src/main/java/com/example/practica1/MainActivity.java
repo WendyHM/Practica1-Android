741117f279db9553c2758b2dbef34e2bf385e9e6
Daniiel314 <daniel314@ciencias.unam.mx>
2022-03-25 19:10:43 -0400

    Comentario agregado

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Juan Daniel Barrera Holan -
+ * <a href="mailto:daniel314@ciencias.unam.mx">daniel314@ciencias.unam.mx</a>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
